f0f6755458c1b7938f39a57e210cbf338c2a07c5
nyaundi254 <nyaundirabin@gmail.com>
2020-11-04 09:19:47 +0300

    First Commit

---
+++ js/index.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+
+    // toggle menu dropdown
+
+$(document).ready(function () {
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar, #content').toggleClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+});
+
+
+
+// Get the modal
+var modal = document.getElementById("profile");
+
+// Get the button that opens the modal
+var btn = document.getElementById("btn_profile");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+})
